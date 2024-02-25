8f2f3feb9958e63827725b7ea558cbb9078372a3
Kaiowesen <102995936+Kaiowesen@users.noreply.github.com>
2024-02-25 19:57:33 +0100

    complete game working

--- javascript.js
+++ javascript.js
@@ -38,8 +38,9 @@ const gameControl = (function () {
     const getGameOver = () => gameOver;
     return { getGameOver, setPlayerSymbolOnBoard, setSelectedRowAndIndex, setGameOver }
 })();
+
 const playerControl = (function () {
-    const playerX = Player("x");
+    const playerX = Player("X");
     const playerY = Player("Y");
 
     let nextPlayer = playerX;
@@ -58,65 +59,73 @@ const playerControl = (function () {
         }
     }
     const getNextPlayer = () => nextPlayer;
+    const getNextPlayerSymbol = () => nextPlayer.symbol
     const getCurrentPlayerSymbol = () => currentPlayer.getPlayerSymbol()
 
-    return { getTurnCount, setTurnCount, setCurrentPlayer, setNextPlayer, getNextPlayer, getCurrentPlayerSymbol, getCurrentPlayerSymbol, getCurrentPlayer }
+    return { getTurnCount, setTurnCount, setCurrentPlayer, setNextPlayer, getNextPlayerSymbol, getNextPlayer, getCurrentPlayerSymbol, getCurrentPlayerSymbol, getCurrentPlayer }
 
 })()
-const playGame = () => {
-    console.log(gameControl.getGameOver() + "game over")
-    if (gameControl.getGameOver() !== true) {
-        playerControl.setCurrentPlayer();
-        playerControl.setNextPlayer();
 
-        let row = prompt("enter row")
-        let index = prompt("enter indez")
-        gameControl.setSelectedRowAndIndex(row, index);
-        gameControl.setPlayerSymbolOnBoard();
-        showBoard()
-        playerControl.setTurnCount()
-        console.log(playerControl.getTurnCount())
-        if (playerControl.getTurnCount() > 4) {
-            console.log("checking winner")
-            checkForGameWinner(playerControl.getCurrentPlayerSymbol())
-        }
-        playGame();
+const domControl = (function () {
+    const messageDiv = document.querySelector(".message-grid");
+    const h2 = document.createElement("h2");
+
+    const markPlayerSymbol = (row, index) => {
+        const h1 = document.createElement("h1");
+        const selectedCell = document.getElementById(row + "-" + index)
+        h1.textContent = playerControl.getCurrentPlayerSymbol()
+        selectedCell.style.display = "grid";
+        h1.style.fontSize = "48px"
+        h1.style.placeSelf = "center"
+        selectedCell.appendChild(h1)
+    }
+    const getCurrentPlayer = () => {
+        playerControl.getTurnCount() % 2 == 0 ? "player Y" : "player X"
+    }
+    const displayPlayerTurnMessage = () => {
+        clearMessageDiv()
+        h2.textContent = `It's player ${playerControl.getTurnCount() % 2 == 0 ? "X" : "Y"}'s turn`
+        messageDiv.appendChild(h2)
+    }
+    const clearMessageDiv = () => {
+        messageDiv.firstElementChild.remove();
     }
 
+    const displayWinnerMessage = () => {
+        h2.style.fontSize = "48px"
+        h2.textContent = `Game over! player ${playerControl.getCurrentPlayerSymbol()} won. Refresh page to play again`
+        messageDiv.appendChild(h2)
+    }
+    return { markPlayerSymbol, displayPlayerTurnMessage, displayWinnerMessage }
+
+})()
+const playRound = (row, index) => {
+
+    if (gameControl.getGameOver() !== true) {
+        playerControl.setCurrentPlayer();
+        gameControl.setSelectedRowAndIndex(row, index);
+        domControl.markPlayerSymbol(row, index)
+        gameControl.setPlayerSymbolOnBoard();
+        playerControl.setTurnCount()
+        domControl.displayPlayerTurnMessage()
+        playerControl.setNextPlayer();
+        if (playerControl.getTurnCount() > 4) {
+            checkForGameWinner(playerControl.getCurrentPlayerSymbol())
+        }
+    }
 }
 const checkForGameWinner = (playerSymbol) => {
     for (let i = 1; i < 4; i++) {
-        console.log("i" + i)
         const rowWinner = gameBoard.board["row" + i][0] == playerSymbol && gameBoard.board["row" + i][1] == playerSymbol && gameBoard.board["row" + i][2] == playerSymbol
         const columnWinner = gameBoard.board.row1[parseInt(i - 1)] == playerSymbol && gameBoard.board.row2[parseInt(i - 1)] == playerSymbol && gameBoard.board.row3[parseInt(i - 1)] == playerSymbol
         const diagonalTopLeftRightWinner = gameBoard.board.row1[0] == playerSymbol && gameBoard.board.row2[1] == playerSymbol && gameBoard.board.row3[2] == playerSymbol
         const diagonalTopRightLeftWinner = gameBoard.board.row1[2] == playerSymbol && gameBoard.board.row2[1] == playerSymbol && gameBoard.board.row3[0] == playerSymbol
 
         if (rowWinner || columnWinner || diagonalTopLeftRightWinner || diagonalTopRightLeftWinner) {
-            console.log("we have a winner")
+            domControl.displayWinnerMessage();
             gameControl.setGameOver()
-            return true
+            return;
         }
-
     }
 };
-const showBoard = () => {
-    console.log(` ${gameBoard.board.row1} \n ${gameBoard.board.row2}  \n ${gameBoard.board.row3}`);
-}
 
-// playerControl.setCurrentPlayer()
-// playerControl.getCurrentPlayer()
-// console.log(playerControl.getCurrentPlayerSymbol())
-// console.log("before")
-// playerControl.setNextPlayer()
-// console.log(playerControl.getNextPlayer());
-// playerControl.setCurrentPlayer()
-
-// console.log("after " + playerControl.getCurrentPlayerSymbol())
-
-
-// playerControl.setNextPlayer()
-
-// console.log("after" + console.log(playerControl.getCurrentPlayer()))
-
-playGame();
